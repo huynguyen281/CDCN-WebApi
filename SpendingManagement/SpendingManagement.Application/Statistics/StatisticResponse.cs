c88c7b7a5d4996668596706a70abe51b8fdf5fd7
legiaanhkhoa <anhkhoalegia.2001@gmail.com>
2022-12-11 19:38:49 +0700

    Webapi: manage statistic

---
+++ SpendingManagement/SpendingManagement.Application/Statistics/StatisticResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpendingManagement.Application.Statistics
+{
+    public class StatisticResponse
+    {
+        public string Time { get; set; }
+        public string SpendMoney { get; set; }
+        public string ReceiveMoney { get; set; }
+    }
+}
